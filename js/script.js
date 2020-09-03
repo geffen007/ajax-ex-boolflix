2d02d55ef0a226c373eab9f5296edad127484925
Guido <guido.amodio@gmail.com>
2020-09-03 18:47:36 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -4,28 +4,24 @@ $(document).ready(function() {
         $('#search').toggle();
         $('.search').toggleClass('whiteB');
         $('input').focus();
-
-        if($('#search').val()!='') {
-            var input = $('#search').val();
-            search(input);
-            reset();
-        }
     });
 
     $('#search').keydown(function () {
-        if(event.which==13){
+        if(event.which == 13 && $('#search').val()!=''){
             var input = $('#search').val();
-            search(input);
+            searchMovie(input);
+            searchTv(input);
             reset();
             $('input#search').val('');
             $('#search').toggle();
             $('.search').removeClass('whiteB');
+            noResult();
         }
     });
 
 });
 
-function search(input){
+function searchMovie(input){
     $.ajax ({
         'url': 'https://api.themoviedb.org/3/search/movie',
         'method': 'GET',
@@ -35,18 +31,30 @@ function search(input){
             'language': 'it-IT'
         },
         'success': function(data) {
-
             var response = data.results;
-
-            if (response.length == 0) {
-                noResult();
-            } else {
-                handlebars(response);
-            }
-
+            handlebars(response);
         },
         'error': function () {
-            noResult();
+            alert('error');
+        },
+    });
+}
+
+function searchTv(input){
+    $.ajax ({
+        'url': 'https://api.themoviedb.org/3/search/tv',
+        'method': 'GET',
+        'data': {
+            'api_key': 'c4427306a0b122697ced9f53faf32324',
+            'query': input,
+            'language': 'it-IT'
+        },
+        'success': function(data) {
+            var response = data.results;
+            handlebars(response);
+        },
+        'error': function () {
+            alert('error');
         },
     });
 }
@@ -60,8 +68,8 @@ function handlebars(resp){
     for (var i = 0; i < resp.length; i++) {
         var context = {
         poster_path: resp[i].poster_path,
-        title: resp[i].title,
-        original_title: resp[i].original_title,
+        title: movieOrTv(resp[i].title, resp[i].name),
+        original_title: movieOrTv(resp[i].original_title, resp[i].original_name),
         original_language: flag(resp[i].original_language),
         altFlag: altFlag(resp[i].original_language),
         vote_average: stars(resp[i].vote_average)
@@ -70,48 +78,58 @@ function handlebars(resp){
         var template = Handlebars.compile(source);
         var html = template(context);
         $('.movies').append(html);
+
+    }
+}
+
+function movieOrTv(resp, resp2){
+    if (resp) {
+        return resp
+    } else {
+        return resp2
     }
 }
 
 function noResult(){
-    var source = $('#no-result-template').html();
-    $('.movies').append(source);
-}
-
-function stars(num){
-    num = num / 2;
-    num = Math.round(num);
-    var star='';
-    for (var i = 0; i < 5; i++) {
-        if (num>i){
-            star+='<i class="fas fa-star"></i>';
-        } else {
-            star+='<i class="far fa-star"></i>';
-        }
+    console.log($('.movie').length);
+    if(($('.movie').length)==0){
+        $('.movies').append($('#no-result-template').html());
     }
-    return star;
 }
 
-// mezza stella
-// function stars (num){
-//     var num = Math.round(num);
-//     var modNum= num%2;
-//     var halfNum= Math.floor(num/2);
-//     console.log(halfNum);
-//
-//     var star= '';
+// function stars(num){
+//     num = num / 2;
+//     num = Math.round(num);
+//     var star='';
 //     for (var i = 0; i < 5; i++) {
-//         if (halfNum > i) {
-//             star += '<i class="fas fa-star"></i>';
-//         } else if(modNum!=0){
-//             star += '<i class="fas fa-star-half-alt"></i>';
-//             modNum=0;
-//         }else {
+//         if (num>i){
+//             star+='<i class="fas fa-star"></i>';
+//         } else {
 //             star+='<i class="far fa-star"></i>';
 //         }
 //     }
 //     return star;
 // }
+
+
+function stars (num){
+    var num = Math.round(num);
+    var modNum= num%2;
+    var halfNum= Math.floor(num/2);
+
+    var star= '';
+    for (var i = 0; i < 5; i++) {
+        if (halfNum > i) {
+            star += '<i class="fas fa-star"></i>';
+        } else if(modNum!=0){
+            star += '<i class="fas fa-star-half-alt"></i>';
+            modNum=0;
+        }else {
+            star+='<i class="far fa-star"></i>';
+        }
+    }
+    return star;
+}
 
 function flag(iso){
     if(iso == 'en'){
@@ -119,7 +137,7 @@ function flag(iso){
     } else if (iso == 'it'){
         return 'it';
     } else {
-        $('p.lang').text('Lingua originale: '+ iso);
+        return iso;
     }
 }
 
@@ -128,7 +146,7 @@ function altFlag(iso){
         return 'bandiera inglese';
     } else if (iso == 'it'){
         return 'bandiera italiana';
-    }else {
-        return;
+    } else {
+        return 'str';
     }
 }
